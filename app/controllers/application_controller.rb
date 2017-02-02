9a130ca52793acf2e79b0cb83a3845110f813a88
Audrey Gehring <audreygehring@gmail.com>
2017-02-02 08:49:25 -0500

    foundation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    shelters_path
+  end
+
   def configure_permitted_parameters
     added_attrs = [
       :first_name,
