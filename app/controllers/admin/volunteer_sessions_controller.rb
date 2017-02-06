9c481b3a75ff256505eab258bb9bccd30649b712
Audrey Gehring <audreygehring@gmail.com>
2017-02-06 17:38:35 -0500

    volunteer session functionality

---
+++ app/controllers/admin/volunteer_sessions_controller.rb
@@ -0,0 +1,25 @@
+class Admin::VolunteerSessionsController < ApplicationController
+  before_action :authenticate_user!, :is_admin?
+
+  def index
+    @volunteer_sessions = VolunteerSession.all
+  end
+
+  def destroy
+    @volunteer_session = VolunteerSession.find(params[:id])
+
+    @volunteer_session.destroy
+    flash[:notice] = "Volunteer session deleted."
+    redirect_to admin_volunteer_sessions_path
+  end
+
+  private
+
+  def is_admin?
+    if current_user.admin?
+      true
+    else
+      flash[:notice] = "Sorry, you don't have access to that feature."
+    end
+  end
+end
