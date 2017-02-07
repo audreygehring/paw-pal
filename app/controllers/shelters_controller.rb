fc01d47f7b49fb1f527899fde8c0ea8f8f533adf
Audrey Gehring <audreygehring@gmail.com>
2017-02-07 10:55:16 -0500

    fix website link

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,37 +1,13 @@
 class SheltersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @volunteer_sessions = VolunteerSession.where(user: current_user)
   end
 
   def show
     if params[:id].length > 5
-      id = params[:id].split('')
-      zip_array = []
-
-      5.times do
-        zip_array << id.pop
-      end
-
-      shelter_key = id.join('')
-      zip_code = zip_array.reverse.join('')
-
-      petfinder_key = ENV["PETFINDER_API_KEY"]
-      uri = URI("http://api.petfinder.com/shelter.get?key=#{petfinder_key}&format=json&location=#{zip_code}&id=#{shelter_key}")
-      shelter_response = Net::HTTP.get_response(uri)
-      shelter_info = JSON.parse(shelter_response.body)
-
-      @shelter_show_data = shelter_info["petfinder"]["shelter"]
-
-      @created_shelter = Shelter.find_or_create_by(name: @shelter_show_data["name"]["$t"],
-                    address: @shelter_show_data["address1"]["$t"],
-                    city: @shelter_show_data["city"]["$t"],
-                    state: @shelter_show_data["state"]["$t"],
-                    zip: @shelter_show_data["zip"]["$t"],
-                    key: shelter_key)
-      @created_shelter.save
-
-      @user = current_user
-      @volunteer_sessions = VolunteerSession.where(shelter: @created_shelter)
+      params_parse(params[:id])
     else
       @created_shelter = Shelter.find(params[:id])
       @volunteer_sessions = VolunteerSession.where(shelter: @created_shelter)
--- app/models/join_shelter.rb
+++
@@ -1,4 +0,0 @@
-class JoinShelter < ApplicationRecord
-  validates :shelter_id, presence: true
-  validates :key_zip, presence: true  
-end
